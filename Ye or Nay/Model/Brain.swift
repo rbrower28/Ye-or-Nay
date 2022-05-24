5fb048ca997f197b3e1ebe2dee3fa715babdf976
Ryan Brower <ryan.brower28@gmail.com>
2022-05-24 16:48:49 -0700

    first commit

---
+++ Ye or Nay/Model/Brain.swift
@@ -0,0 +1,8 @@
+//
+//  Brain.swift
+//  Ye or Nay
+//
+//  Created by Ryan Brower on 5/24/22.
+//
+
+import Foundation
---
+++ Ye or Nay/Model/Quote.swift
@@ -0,0 +1,8 @@
+//
+//  Quote.swift
+//  Ye or Nay
+//
+//  Created by Ryan Brower on 5/24/22.
+//
+
+import Foundation

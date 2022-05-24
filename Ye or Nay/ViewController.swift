e56641fae946f4bd13be3bfd1a0aec94c972c78f
Ryan Brower <ryan.brower28@gmail.com>
2022-05-24 14:41:51 -0700

    Initial Commit

---
+++ Ye or Nay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ye or Nay
+//
+//  Created by Ryan Brower on 5/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ye or NayUITests/Ye_or_NayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ye_or_NayUITestsLaunchTests.swift
+//  Ye or NayUITests
+//
+//  Created by Ryan Brower on 5/24/22.
+//
+
+import XCTest
+
+class Ye_or_NayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
